2dc82caa2c377bb5ee5069ff485cae9a4a948d53
MANNANTHIND.MT <MANNANTHIND.MT@gmail.com>
2020-03-23 11:17:59 +1300

    Add project files.

---
+++ MyRRgame/GameForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MyRRgame
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load = new System.Windows.Forms.Button();
+            this.Spin = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Fire = new System.Windows.Forms.Button();
+            this.FireAway = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_PlayAgain = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Load.ForeColor = System.Drawing.Color.Black;
+            this.Load.Location = new System.Drawing.Point(12, 33);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(166, 90);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "LOAD";
+            this.Load.UseVisualStyleBackColor = false;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Spin
+            // 
+            this.Spin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Spin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Spin.ForeColor = System.Drawing.Color.Black;
+            this.Spin.Location = new System.Drawing.Point(12, 129);
+            this.Spin.Name = "Spin";
+            this.Spin.Size = new System.Drawing.Size(166, 92);
+            this.Spin.TabIndex = 1;
+            this.Spin.Text = "SPIN";
+            this.Spin.UseVisualStyleBackColor = false;
+            this.Spin.Click += new System.EventHandler(this.Spin_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.Color.Black;
+            this.Exit.Location = new System.Drawing.Point(217, 356);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(105, 65);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "EXIT";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Fire
+            // 
+            this.Fire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Fire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fire.ForeColor = System.Drawing.Color.Black;
+            this.Fire.Location = new System.Drawing.Point(12, 227);
+            this.Fire.Name = "Fire";
+            this.Fire.Size = new System.Drawing.Size(166, 86);
+            this.Fire.TabIndex = 3;
+            this.Fire.Text = "FIRE";
+            this.Fire.UseVisualStyleBackColor = false;
+            this.Fire.Click += new System.EventHandler(this.Fire_Click);
+            // 
+            // FireAway
+            // 
+            this.FireAway.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.FireAway.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FireAway.ForeColor = System.Drawing.Color.Black;
+            this.FireAway.Location = new System.Drawing.Point(12, 319);
+            this.FireAway.Name = "FireAway";
+            this.FireAway.Size = new System.Drawing.Size(166, 90);
+            this.FireAway.TabIndex = 4;
+            this.FireAway.Text = "FIRE AWAY";
+            this.FireAway.UseVisualStyleBackColor = false;
+            this.FireAway.Click += new System.EventHandler(this.FireAway_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(197, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 305);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_PlayAgain
+            // 
+            this.btn_PlayAgain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn_PlayAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_PlayAgain.ForeColor = System.Drawing.Color.Black;
+            this.btn_PlayAgain.Location = new System.Drawing.Point(387, 356);
+            this.btn_PlayAgain.Name = "btn_PlayAgain";
+            this.btn_PlayAgain.Size = new System.Drawing.Size(149, 65);
+            this.btn_PlayAgain.TabIndex = 6;
+            this.btn_PlayAgain.Text = "Play Again";
+            this.btn_PlayAgain.UseVisualStyleBackColor = false;
+            this.btn_PlayAgain.Click += new System.EventHandler(this.btn_PlayAgain_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_PlayAgain);
+            this.Controls.Add(this.FireAway);
+            this.Controls.Add(this.Fire);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Spin);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Name = "GameForm";
+            this.Text = "Russian Rollette";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Spin;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Fire;
+        private System.Windows.Forms.Button FireAway;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_PlayAgain;
+    }
+}
+
---
+++ MyRRgame/GameForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace MyRRgame
+{
+    public partial class GameForm : Form
+    {
+        MyClass logic = new MyClass();
+        public GameForm()
+        {
+            InitializeComponent();
+            Spin.Enabled = false;
+            Fire.Enabled = false;
+            FireAway.Enabled = false;
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = MyRRgame.Resource1.load;
+            SoundPlayer sc = new SoundPlayer(MyRRgame.Resource1.loadvoice);
+            sc.Play();//put a sound
+            logic.LoadGun();
+            Load.Enabled = false;
+            Spin.Enabled = true;
+            Fire.Enabled = false;
+            FireAway.Enabled = false;
+        }
+
+        private void Spin_Click(object sender, EventArgs e)
+        {
+            Load.Enabled = true;
+            pictureBox1.Image = MyRRgame.Resource1.spin;
+            SoundPlayer sc = new SoundPlayer(MyRRgame.Resource1.spinvoice);
+            sc.Play();
+            Fire.Enabled = true;
+            FireAway.Enabled = true;
+            logic.SpinGun();// calling the function to work
+            Load.Enabled = false;
+            Spin.Enabled = false;
+            Fire.Enabled = true;
+            FireAway.Enabled = true;
+        }
+
+        private void Fire_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = MyRRgame.Resource1.fire;
+            SoundPlayer sc = new SoundPlayer(MyRRgame.Resource1.firevoice);
+            sc.Play();
+            int chances = logic.FireGun();
+        }
+
+        private void FireAway_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = MyRRgame.Resource1.fireaway;
+            SoundPlayer sc = new SoundPlayer(MyRRgame.Resource1.firevoice);
+            sc.Play();
+            int ChancesAway = logic.FireAwayGun();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_PlayAgain_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
